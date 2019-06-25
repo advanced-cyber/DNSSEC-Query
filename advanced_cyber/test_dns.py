ebac70098403843833d27676396d33064d1b3e63
Shirbt <33618364+Shirbt@users.noreply.github.com>
2019-06-25 06:44:45 -0400

    mega super puper Shir

--- advanced_cyber/test_dns.py
+++ advanced_cyber/test_dns.py
@@ -27,6 +27,11 @@ import numpy as np
 cmdig = "dig +dnssec"
 cmdtrace = "dig +trace"
 
+# CSV File
+loc = 'dnssec.csv'
+
+
+
 # this function checks flag field of DNSSEC
 def checkDNSSEC(command):
     p = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE)
@@ -64,63 +69,65 @@ def traceDNSSEC(command, key):
                     name = workline1[0]
                     raddress = workline2[5]
                     query_flag = False            # no dnssec in trace, if all rows reach this stage with secflag=1, the query_flag will be 1
-                    # print("Algorithm: "+"Null"+" Name: "+workline1[0])
-                    # print("Raddress: "+workline2[5])
                 else:
                     algorithm = workline1[5]
                     name = workline1[0]
                     raddress = workline2[5]
                     if (workline1[3] == 'RRSIG'):
                         secflag = 1
-                    # print("Algorithm: "+workline1[5]+" Name: "+workline1[0])
-                    # print("Raddress: "+workline2[5])
             except Exception:
                 algorithm = -1
                 name = workline1[0]
                 raddress = workline2[5]
                 query_flag = False            # no dnssec in trace, if all rows reach this stage with secflag=1, the query_flag will be 1
-                # print("Algorithm: "+"Null"+" Name: "+workline1[0])
-                # print("Raddress: "+workline2[5])
-            # insert into traceDATA object and print
-            # print('Workline1: ', workline1)
-            # print('Workline2: ', workline2)
             a = traceDATA(key, name, secflag, algorithm, raddress)
             rows.append(a)
             secflag = 0 
-            a.prinTrace()
         oldline = items
     return (query_flag, rows)
 
 if __name__ == "__main__":
-    with open('overall_results.csv', mode='w') as overall_results:
-        with open('trace_results.csv', mode='w') as trace_results:
-            site =  "www.ariel.ac.il"
-            DIGcommand = cmdig + ' ' + site
-            TRACEcommand = cmdtrace + ' ' + site
+    with open(loc) as csv_sites:
+        with open('overall_results.csv', mode='w') as overall_results:
+            with open('trace_results.csv', mode='w') as trace_results:
+
+                # defining headers of csv files
+                overall_fields = ["Website", "Dig DNSSEC Flag", "Trace Query Flag"]
+                trace_fields = ["Website", "Partial Query", "SecFlag", "Algorithm", "Received Address"]
+
+                # write header of overall_results
+                overall_writer = csv.DictWriter(overall_results, fieldnames = overall_fields,  extrasaction='ignore', delimiter = ';')
+                overall_writer.writeheader()
+                # write header of trace_results
+                trace_writer = csv.DictWriter(trace_results, fieldnames = trace_fields,  extrasaction='ignore', delimiter = ';')
+                trace_writer.writeheader()
+
+                # read site csv file
+                read_csv_sites = csv.reader(csv_sites, delimiter=',')
+
+                # skip first row (fields)
+                next(read_csv_sites)
+
+                # for row in sites csv
+                for (i, row) in enumerate(read_csv_sites):
+
+                    site =  row[1]
+                    DIGcommand = cmdig + ' ' + site
+                    TRACEcommand = cmdtrace + ' ' + site
      
-            # defining headers of csv files
-            overall_fields = ["Website", "Dig DNSSEC Flag", "Trace Query Flag"]
-            trace_fields = ["Website", "Partial Query", "SecFlag", "Algorithm", "Received Address"]
+                    # dig command for current website
+                    flag = checkDNSSEC(DIGcommand)
 
-            # write header of overall_results
-            overall_writer = csv.DictWriter(overall_results, fieldnames = overall_fields,  extrasaction='ignore', delimiter = ';')
-            overall_writer.writeheader()
-            # write header of trace_results
-            trace_writer = csv.DictWriter(trace_results, fieldnames = trace_fields,  extrasaction='ignore', delimiter = ';')
-            trace_writer.writeheader()
-
-            flag = checkDNSSEC(DIGcommand)
-            if (flag):
-                print("DNSSEC")
-            else:
-                print("NO DNSSEC")
-
-            # trace overall answer is held in trace_ans
-            trace_ans, rows = traceDNSSEC(TRACEcommand, site)
+                    # trace overall answer is held in trace_ans
+                    trace_ans, rows = traceDNSSEC(TRACEcommand, site)
            
-            for row in rows:
-                trace_writer.writerows([{'Website': row.getKey(), 'Partial Query': row.getName(), 'SecFlag': row.getSecFlag(), 'Algorithm': row.getAlgorithm(), 'Received Address': row.getRAddress()}])
+                    for row in rows:
+                        trace_writer.writerows([{'Website': row.getKey(), 'Partial Query': row.getName(), 'SecFlag': row.getSecFlag(), 'Algorithm': row.getAlgorithm(), 'Received Address': row.getRAddress()}])
 
-            # writing overall answers into overall_results csv file
-            overall_writer.writerows([{'Website': site, 'Dig DNSSEC Flag': flag, 'Trace Query Flag': trace_ans}])
+                    # writing overall answers into overall_results csv file
+                    overall_writer.writerows([{'Website': site, 'Dig DNSSEC Flag': flag, 'Trace Query Flag': trace_ans}])
+                    
+                    # show an update every 50 websites
+                    if (i > 0 and i % 50 == 0):
+                        print("[INFO] processed {}/{}".format(i, 544))
 
